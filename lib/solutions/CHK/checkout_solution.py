5f9a0639b4f4cf3ca9903186a86a4a5951e1a14e
sbx_user1051 <sbx_user1051@169.254.19.221>
2025-08-20 13:45:30 +0000

    Wed Aug 20 13:45:30 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -14,45 +14,35 @@ class CheckoutSolution:
             
         prices = {'A': 50, 'B': 30, 'C': 20, 'D': 15, 'E': 40}
         offers = {
-            'A': [(3, 130), (5, 200)],
+            'A': [(5, 200), (3, 130)],
             'B': [(2, 45)]
             }
-        cross_offers = [
-            {'source': 'E', 'req': 2, 'target': 'B', 'free': 1}
-        ]
-
+        
         counts = Counter(skus)
 
-        # Handle cross item to calc free adjustments 
-        free_adjust = {}
-        for offer in cross_offers:
-            source_count = counts.get(offer['source'], 0)
-            target_count = counts.get(offer['target'], 0)
-            if source_count > 0 and target_count > 0:
-                num_apps = min(source_count // offer['req'], target_count // offer['free'])
-                free_adjust[offer['target']] = free_adjust.get(offer['target'], 0) + num_apps * offer['free']
-
-
+        #cross_offers = [
+        #    {'source': 'E', 'req': 2, 'target': 'B', 'free': 1}
+        #]
+        free_b = min(counts.get('B',0), counts.get('E', 0) // 2)
         total = 0
 
         for item, price in prices.items():
             count = counts.get(item, 0)
-            # free adjustments cross offer stuff
-            payable = count - free_adjust.get(item, 0)
-            if payable < 0:
-                payable = 0 # prevents min
+            if item == 'B':
+                count -= free_b
 
-
-            if item in offers and offer[item]:
-                item_offers = sorted(offers[item], key=lambda x: x[0], reverse=True)
-                rem = payable
-                for qty, offer_price in item_offers:
-                    num = rem // qty
-                    total += num * offer_price
-                    rem %= qty
-                total += rem * price
+            if item in offers:
+                sorted_offers = sorted(offers[item], key=lambda x: x[0], reverse=True)
+                sub_count = count
+                item_total = 0
+                for qty, offer_price in sorted_offers:
+                    item_total += (sub_count //  qty)*offer_price
+                    sub_count %=qty
+                item_total += sub_count*price
+                total += item_total
             else:
-                total += payable * price
+                total += count * price
         return total
+
 
 
