3e49c9571abe3b1838276b435cf7448c1f1335e6
sbx_user1051 <sbx_user1051@169.254.19.221>
2025-08-20 14:28:41 +0000

    Wed Aug 20 14:28:41 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -9,32 +9,55 @@ class CheckoutSolution:
         if not isinstance(skus, str):
             return -1
         for char in skus:
-            if char not in 'ABCDEF':
+            if char not in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
                 return -1
             
-        prices = {'A': 50, 'B': 30, 'C': 20, 'D': 15, 'E': 40, 'F': 10}
+        prices = {
+            'A': 50, 'B': 30, 'C': 20, 'D': 15, 'E': 40, 'F': 10,
+            'G': 20, 'H': 20, 'I': 35, 'J': 60, 'K': 80, 'L': 90,
+            'M': 15, 'N': 40, 'O': 10, 'P': 50, 'Q': 30, 'R': 50,
+            'S': 30 ,'T': 20, 'U': 40, 'V': 50, 'W': 20, 'X': 90,
+            'Y': 10, 'Z': 50
+            }
+        
         offers = {
             'A': [(5, 200), (3, 130)],
-            'B': [(2, 45)]
+            'B': [(2, 45)],
+            'H': [(10, 80), (5, 45)],
+            'K': [(2, 150)],
+            'P': [(5, 200)],
+            'Q': [(3, 80)],
+            'V': [(3, 180), (2, 90)]
             }
         
         counts = Counter(skus)
 
-        #cross_offers = [
-        #    {'source': 'E', 'req': 2, 'target': 'B', 'free': 1}
-        #]
-        free_b = min(counts.get('B',0), counts.get('E', 0) // 2)
+        # These are the cross items
+        free_b = min(counts.get('B', 0), counts.get('E', 0) // 2)
+        free_q = min(counts.get('Q', 0), counts.get('Q', 0) // 3)
 
-        #new offer
+        # Same items free offer
         free_f = counts.get('F', 0)//3
+        free_n = counts.get('N', 0)//4
+        free_u = counts.get('U', 0)//4
+
+        #Dict for frree items to stop if repeating loop
+        free_counts = {
+            'B': free_b,
+            'F': free_f,
+            'N': free_n,
+            'Q': free_q,
+            'U': free_u
+        }
+
         total = 0
 
         for item, price in prices.items():
             count = counts.get(item, 0)
-            if item == 'B':
-                count -= free_b
-            if item =='F':
-                count -= free_f
+            count -= free_counts.get(item, 0)
+            if count < 0:
+                count = 0
+        
             if item in offers:
                 sorted_offers = sorted(offers[item], key=lambda x: x[0], reverse=True)
                 sub_count = count
@@ -47,3 +70,4 @@ class CheckoutSolution:
             else:
                 total += count * price
         return total
+
